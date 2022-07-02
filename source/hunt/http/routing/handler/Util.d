cb3565a24d4e7187189e31edfe4fe5525719adad
Malte Jürgens <maltejur@dismail.de>
2022-07-02 21:24:03 +0200

    fix

--- source/hunt/http/routing/handler/Util.d
+++ source/hunt/http/routing/handler/Util.d
@@ -108,6 +108,7 @@ struct RoutingHandlerUtils {
         int remainingSize = rangeEnd.to!uint - rangeStart.to!uint + 1;
         if(remainingSize <= 0) {
             warningf("actualSize:%d, remainingSize=%d", fileSize, remainingSize);
+            context.write(cast(byte[])[]);
         } else {
             auto buf = f.rawRead(new ubyte[remainingSize]);
             // response.setContent(buf);
